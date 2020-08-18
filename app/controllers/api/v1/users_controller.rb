2ec53aa0c849579f959d2ede974f0dc81f0d3933
Michael <michaeljanschiumo@gmail.com>
2020-08-17 20:26:43 -0400

    added user create action and routes

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,12 @@
 class Api::V1::UsersController < ApplicationController
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in(@user)
+      render json: @user, status: 200
+    else
+      render json: { errors: @user.errors.full_messages }, status: 400
+    end
   end
 end
