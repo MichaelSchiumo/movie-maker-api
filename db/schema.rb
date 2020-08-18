dd6f80835c2508236a1048dd66fb939d939d3cac
Michael <michaeljanschiumo@gmail.com>
2020-08-18 11:18:03 -0400

    added movie_id to reviews table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_18_150745) do
+ActiveRecord::Schema.define(version: 2020_08_18_151708) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2020_08_18_150745) do
     t.integer "score"
     t.text "comment"
     t.integer "user_id"
+    t.integer "movie_id"
   end
 
   create_table "users", force: :cascade do |t|
