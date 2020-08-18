ab683aaa80a827910e7ef25e896ce42b86e2bda6
Michael <michaeljanschiumo@gmail.com>
2020-08-17 20:33:57 -0400

    user signup functionality working

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -9,4 +9,10 @@ class Api::V1::UsersController < ApplicationController
       render json: { errors: @user.errors.full_messages }, status: 400
     end
   end
+
+  private 
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
 end
