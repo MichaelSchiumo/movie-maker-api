036b251bbd1f930667ae55354f549f7c898c8ecc
Michael <michaeljanschiumo@gmail.com>
2020-08-17 22:11:45 -0400

    created sessions controller, added login functionality

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,12 @@
+class Api::V1::SessionsController < ApplicationController
+
+  def create
+    @user = User.find_by(username: params[:user][:username])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id 
+      render json: @user, status: 200
+    else 
+      render json: { errors: ['Unable to login.'] }, status: 400
+    end
+  end
+end
